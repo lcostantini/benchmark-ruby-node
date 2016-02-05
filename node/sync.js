4511c78c623abef2aa4aef5a92fe806cefd17cf3
leolowerbina <leonardo@bina.com>
2016-02-05 14:14:33 -0300

    Add Sync and Async examples

---
+++ node/sync.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const http = require('http');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
+const PORT = 8090;
+
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  var server = http.createServer(handleRequest);
+
+  server.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:%s", PORT);
+  });
+}
+
+function handleRequest(request, response) {
+  var data = fs.readFileSync('../test.txt', 'utf8');
+  response.end(new Array(1000).join(data));
+}
